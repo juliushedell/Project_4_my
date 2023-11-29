e0a35d3b953dd39ca56539d03a4e98ad7dadf63d
juliushedell <julius.hedell.9951@gmail.com>
2023-11-29 10:34:50 +0100

    Silence before the storm

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -3,7 +3,50 @@
 </template>
 
 <script>
+import ResponsiveNav from '@/components/ResponsiveNav.vue';
+import io from 'socket.io-client';
+const socket = io("localhost:3000");
+
+export default {
+  name: 'StartView',
+  components: {
+    ResponsiveNav
+  },
+  data: function () {
+    return {
+      uiLabels: {},
+      id: "",
+      lang: localStorage.getItem("lang") || "en",
+      hideNav: true
+    }
+  },
+  created: function () {
+    socket.emit("pageLoaded", this.lang);
+    socket.on("init", (labels) => {
+      this.uiLabels = labels
+    })
+  },
+  methods: {
+    switchLanguage: function() {
+      if (this.lang === "en") {
+        this.lang = "sv"
+      }
+      else {
+        this.lang = "en"
+      }
+      localStorage.setItem("lang", this.lang);
+      socket.emit("switchLanguage", this.lang)
+    },
+    toggleNav: function () {
+      this.hideNav = ! this.hideNav;
+    },
+    generateGameCode: function () {
+    return Math.floor(Math.random() * 100000);
+  }
+  }
+}
 </script>
+
 
 <style>
 </style>
