7a6b6479ab82950f466b620f33db1a5d5e0914d9
juliushedell <julius.hedell.9951@gmail.com>
2023-12-12 16:05:47 +0100

    emelie

--- src/views/Podium.vue
+++ src/views/Podium.vue
@@ -1,16 +1,71 @@
 <template>
-<p> Podium </p>
+    <header> 
+        <h1>
+            Podium
+            <img src="/img/Head_picture.png" class="head_picture">
+        </h1>
+    </header>
+        <div class="answerDisplay">
+        <h3>
+            {{ uiLabels['answer'] }}
+        </h3>
+    </div>
 
-
-<div class="answerDisplay"> 
-     
-</div>
 
 </template>
 
 <script>
 
+import ResponsiveNav from '@/components/ResponsiveNav.vue';
+import io from 'socket.io-client';
+const socket = io("localhost:3000");
 
+export default {
+name: 'LobbytwoView',
+components: {
+  ResponsiveNav
+},
+data: function () {
+  return {
+    uiLabels: {},
+    id: "",
+    lang: localStorage.getItem("lang") || "en",
+    hideNav: true,
+    // lagrar confessions i array
+    conf:[],
+    poll: {},
+    gameCode: 0,
+    players: {}, 
+    isInputDisabled: false //grundvariabel som gör att det går att redigera i iinput fieldsen
+  }
+},
+created: function () {
+  this.gameCode = this.$route.params.gameCode
+  socket.emit("pageLoaded", this.lang);
+  socket.emit("getPoll", this.gameCode);
+  socket.emit("getPlayers", this.gameCode);
+  socket.on("pullPlayer", (players) => {
+    this.players = players
+  })
+  socket.on("pullPoll", (poll) => {
+    this.poll = poll
+  })
+  socket.on("init", (labels) => {
+    this.uiLabels = labels
+  })
+},
+methods: {
+  switchLanguage: function() {
+    if (this.lang === "en") {
+      this.lang = "sv"
+    }
+    else {
+      this.lang = "en"
+    }
+    localStorage.setItem("lang", this.lang);
+    socket.emit("switchLanguage", this.lang)
+  },
+ }}
 </script>
 
 <style>
