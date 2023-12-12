3b2e5a57c3c9f6c88425476a3163455c2421e4eb
emelie987 <emelie.edberg987@gmail.com>
2023-12-12 15:55:07 +0100

    uppdatering

--- src/views/Podium.vue
+++ src/views/Podium.vue
@@ -1,9 +1,23 @@
 <template>
 <p> Podium </p>
+
+
+<div class="answerDisplay"> 
+     
+</div>
+
 </template>
 
 <script>
+
+
 </script>
 
 <style>
+.answerDisplay {
+    font-size: 22px;
+    color: green;
+}
+
+
 </style>
