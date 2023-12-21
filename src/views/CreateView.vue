c2f605b43a60603205242761220d45f92709f1ae
annaruhn <ruhnanna@gmail.com>
2023-12-21 16:37:32 +0100

    tagit bort onödigt

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -45,11 +45,11 @@
     </div>
   
   <div class="themebuttons">
-    <input type="radio" id="childhood" v-model="theme" name="the_theme" :value="uiLabels['Childhood']" @change="updateTheme('Childhood')"/>
+    <input type="radio" id="childhood" v-model="theme" name="the_theme" :value="uiLabels['Childhood']"/>
     <label class="themes" for="childhood">{{ uiLabels["Childhood"] }}</label>
-    <input type="radio" id="illegal" v-model="theme" name="the_theme" :value="uiLabels['Illegal']" @change="updateTheme('Illegal')"/>
+    <input type="radio" id="illegal" v-model="theme" name="the_theme" :value="uiLabels['Illegal']"/>
     <label class="themes" for="illegal">{{ uiLabels["Illegal"] }}</label>
-    <input type="radio" id="uti" v-model="theme" name="the_theme" :value="uiLabels['Under the influence']" @change="updateTheme('Under the influence')"/>
+    <input type="radio" id="uti" v-model="theme" name="the_theme" :value="uiLabels['Under the influence']"/>
     <label class="themes" for="uti">{{ uiLabels["Under the influence"] }}</label>
     <p class="ot">{{ uiLabels["Own theme: "] }}</p>
     <input type="text" class="otherTheme" v-model="theme" :maxlength="35" :class="{'invalid-input': (!this.theme.length > 0) && buttonClicked}" required/>
@@ -102,9 +102,6 @@
       )
   },
   methods: {
-    updateTheme(themeKey) {
-      this.theme = this.uiLabels[themeKey];
-    },
     checkNameLength() {
       if (this.name.length === 15) {
         this.showAlert = true; 
