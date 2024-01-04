f5b2a5fe4bffb9fcdc53450b05c88cccf8d90033
hugotenerz <h.tenerz@gmail.com>
2024-01-04 11:34:57 +0100

    SV

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,33 +1,30 @@
 <template>
-  <header>
-    <div id="languageWrapper">
-      <button v-on:click="switchLanguageEnglish" id="language_button">
+  <div id="languageWrapper">
+    <button v-on:click="switchLanguageEnglish" id="language_button">
       <img src="img/britishFlagga.png" id="language_button">
-      </button>
-      <button v-on:click="switchLanguageSwedish" id="language_button">
+    </button>
+    <button v-on:click="switchLanguageSwedish" id="language_button">
       <img src="img/svenskFlagga.png" id="language_button">
-      </button>
-    </div>
-  </header>
-    <div id="frontText">
-      <h2>ALLEGATIONS</h2>
-      <h4>{{ uiLabels['sales-pitch'] }}</h4> 
-    <img  src="img/alligatorStart.png" id="startpage_picture">
-    </div>
-    <disp class=wrapper>
-     <router-link to="/create/" class="button" style="grid-area:a;">{{uiLabels.host}}</router-link>
-     <router-link to="/JoinGameCode/" class="button" style="grid-area:b;">{{uiLabels.joinGame}}</router-link>
-    </disp>
-
-    <disp class=rightButton>
-    <router-link to="/HowToPlayView/" class="button" style="grid-area:d;">{{uiLabels.howToPlay}}</router-link>
-    </disp>
+    </button>
+  </div>
+  <div id="frontText">
+    <h2>ALLEGATIONS</h2>
+    <h4>{{ uiLabels['sales-pitch'] }}</h4>
+    <img src="img/alligatorStart.png" id="startpage_picture">
+  </div>
+  <disp class=wrapper>
+    <router-link to="/create/" class="button" style="grid-area:a;">{{ uiLabels.host }}</router-link>
+    <router-link to="/JoinGameCode/" class="button" style="grid-area:b;">{{ uiLabels.joinGame }}</router-link>
+  </disp>
+  <disp class=rightButton>
+    <router-link to="/HowToPlayView/" class="button" style="grid-area:d;">{{ uiLabels.howToPlay }}</router-link>
+  </disp>
 </template>
 
 <script>
 import io from 'socket.io-client';
 // sessionStorage.setItem("dataServer", "192.168.1.209:3000") // Julius IP 
-sessionStorage.setItem("dataServer", "localhost:3000") 
+sessionStorage.setItem("dataServer", "localhost:3000")
 const socket = io(sessionStorage.getItem("dataServer"));
 
 export default {
@@ -45,12 +42,12 @@ export default {
     })
   },
   methods: {
-    switchLanguageEnglish: function() {
+    switchLanguageEnglish: function () {
       this.lang = "en"
       localStorage.setItem("lang", this.lang);
       socket.emit("switchLanguage", this.lang)
     },
-    switchLanguageSwedish: function() {
+    switchLanguageSwedish: function () {
       this.lang = "sv"
       localStorage.setItem("lang", this.lang);
       socket.emit("switchLanguage", this.lang)
@@ -60,46 +57,12 @@ export default {
 </script>
 
 <style scoped>
-  header {
-    background-color: #81b8ce ;
-    width: 100%;
-    text-align: right;
-  }
-
-  .wrapper{
-  display: flex;
-  justify-content: center;
-  gap: 20px; 
-  margin-top: 10px;
-}
-
-.rightButton{
+#languageWrapper {
   display: flex;
   justify-content: right;
+  gap: 10px;
   margin-top: 20px;
-  padding-right: 5px;
-}
-
-h2 {
-  font-size: 3vw;
-  font-family: monospace;
-  letter-spacing: 0.4vw;
-}
-
-h4{
-  font-size: 1.5vw;
-  font-family: monospace;
-  letter-spacing: 0.1vw;
-}
-
-body{
-    background-color: #81b8ce; 
-  }
-
-#frontText{
-  color: #2a9451;
-  font-family: monospace; 
-  text-align: center;
+  margin-right: 30px;
 }
 
 #language_button {
@@ -109,22 +72,38 @@ body{
   height: 40px;
 }
 
-#languageWrapper {
-  display: flex;
-  justify-content: right;
-  gap: 10px;
-  margin-top: 20px;
-  margin-right: 30px;
+#language_button:hover {
+  cursor: pointer;
 }
 
-#language_button:hover {
-      cursor: pointer;
-    }
+#frontText {
+  color: #2a9451;
+  font-family: monospace;
+  text-align: center;
+}
+
+.wrapper {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 10px;
+}
+
+.rightButton {
+  display: flex;
+  justify-content: right;
+  margin-top: 20px;
+  padding-right: 5px;
+}
+
+body {
+  background-color: #81b8ce;
+}
 
 #startpage_picture {
   margin-bottom: 10px;
-  width:400px; 
-  height:250px;
+  width: 400px;
+  height: 250px;
 }
 
 .button {
@@ -132,47 +111,42 @@ body{
 }
 
 @media screen and (max-width:50em) {
-.rightButton{
-  justify-content: center;
-}
-.button{
-  width: 18vw; 
-  height: 6vh; 
-}
+  .rightButton {
+    justify-content: center;
+  }
 
-h2 {
-  font-size: 26px;
-  font-family: monospace;
-  letter-spacing: 2px;
-} 
+  .button {
+    width: 18vw;
+    height: 6vh;
+  }
 
-h4{
-  font-size: 13px;
-  font-family: monospace;
-letter-spacing: 1.0px;
+  #startpage_picture {
+    width: 266px;
+    height: 166px;
+  }
 }
-
-#startpage_picture {
-  width: 266px;
-  height: 166px;
-}}
 
 @media only screen and (max-width: 2532px) and (orientation: portrait) {
   h2 {
-  padding-top: 75px;
-} 
+    padding-top: 75px;
+    font-size: 26px;
+    font-family: monospace;
+    letter-spacing: 2px;
+  }
+
+  h4 {
+    font-size: 13px;
+    font-family: monospace;
+    letter-spacing: 1.0px;
+  }
 
   #startpage_picture {
-  margin-bottom: 20px;
-  width:177,8px; 
-  height:111,1px;
+    margin-bottom: 20px;
+  }
+
+  .button {
+    width: 30vw;
+    height: 6vh;
+  }
 }
-
-.button{
-  width: 30vw; 
-  height: 6vh; 
-}}
-
-
-
 </style>
